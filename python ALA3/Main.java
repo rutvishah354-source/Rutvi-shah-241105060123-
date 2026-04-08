2a232e99a15754cbffdf114dda29b6b86fa892a3
rutvishah354-source <rutvishah354@gmail.com>
2026-04-08 18:52:47 +0530

    Add files via upload

---
+++ python ALA3/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+ public static void main(String[] args) {
+ int a=10, b=25, c=15;
+ if(a>b && a>c)
+ System.out.println("Largest: " + a);
+ else if(b>c)
+ System.out.println("Largest: " + b);
+ else
+ System.out.println("Largest: " + c);
+ }
+}
